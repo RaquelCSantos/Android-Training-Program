2e682c0f18a13ff5a9135a0183e81952bd9ec924
RaquelCSantos <47529436+RaquelCSantos@users.noreply.github.com>
2020-11-10 16:37:45 +0000

    inicial commit

---
+++ Ex2_Aula2/app/src/main/java/pt/atp/ex3_aula2/DetailsActivity.kt
@@ -0,0 +1,11 @@
+package pt.atp.ex3_aula2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
