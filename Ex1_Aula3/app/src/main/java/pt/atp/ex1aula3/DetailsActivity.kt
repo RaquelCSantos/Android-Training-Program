01f0a7a8b8934e12d9c562098d118debf442c259
RaquelCSantos <47529436+RaquelCSantos@users.noreply.github.com>
2020-11-15 15:41:09 +0000

    initial commit

---
+++ Ex1_Aula3/app/src/main/java/pt/atp/ex1aula3/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package pt.atp.ex1aula3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
---
+++ Ex1_Aula3/app/src/main/java/pt/atp/ex1aula3/PlaygroundActivity.kt
@@ -0,0 +1,7 @@
+package pt.atp.ex1aula3
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PlaygroundActivity : AppCompatActivity() {
+
+}
